65cf07ad27e5ab22fdd66605c67309e5bc88abe1
Hidde <hiddebalestra@gmail.com>
2024-02-21 11:46:29 +0100

    added load.js

---
+++ load.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+    // Your code here, it will run when the entire page is loaded
+    // Inside your loadChannels function or script file
+    document.getElementById('submitCategoryData').addEventListener('click', function() {
+        submitCategoryData()
+    })
+    document.getElementById('showFeedPopup').addEventListener('click', function() {
+        showFeedPopup()
+    })
+    document.getElementById('submitChannelData').addEventListener('click', function() {
+        submitChannelData()
+    })
+    document.getElementById('backToChannels').addEventListener('click', function() {
+        backToChannels()
+    })
+    document.getElementById('backToCategories').addEventListener('click', function() {
+        backToCategories()
+    })
+    document.getElementById('exportChannelsBtn').addEventListener('click', function() {
+        exportChannels()
+    })
+    document.getElementById('importChannelsBtn').addEventListener('click', function() {
+        importChannels()
+    })
+    document.getElementById('refresh').addEventListener('click', function() {
+        getRssFeeds(selChannel.url, selChannel.name, true)
+    })
+
+    loadCategories()
+    loadChannels()
+    bestYoutubeInstance()
+}
