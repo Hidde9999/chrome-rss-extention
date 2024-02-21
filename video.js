6f0e31dea4a652330dadaabdf6ac1d43ae9184b9
Hidde <hiddebalestra@gmail.com>
2024-02-21 11:16:41 +0100

    remove ; and added video.link = video.link.replace("www.youtube.com", bestYoutubeSite)

--- video.js
+++ video.js
@@ -8,73 +8,80 @@ function saveVideo(title, link, date) {
         link: link,
         date: date,
         isFavorite: false
-    });
+    })
 }
 
 function saveVideoList(name, refresh) {
     if (refresh) {
-        updateVideoList(name);
+        updateVideoList(name)
     } else {
-        videoList = tempVideoList.slice();
-        cookieSave(name);
+        videoList = tempVideoList.slice()
+        cookieSave(name)
     }
 }
 
 function updateVideoList(name) {
-    tempVideoList.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+    tempVideoList.sort((a, b) => Date.parse(b.date) - Date.parse(a.date))
     tempVideoList.forEach((item) => {
         if (videoList.length > 0 && Date.parse(item.date) > Date.parse(videoList[0].date)) {
-            videoList.push(item);
+            videoList.push(item)
         }
     });
-    cookieSave(name);
+    cookieSave(name)
 }
 
 function cookieSave(name) {
-    videoList.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
-    localStorage.setItem(name, JSON.stringify(videoList));
-    tempVideoList = [];
-    loadVideo(name);
+    videoList.sort((a, b) => Date.parse(b.date) - Date.parse(a.date))
+    localStorage.setItem(name, JSON.stringify(videoList))
+    tempVideoList = []
+    loadVideo(name)
 }
 
 function loadVideo(name) {
-    const videosList = document.getElementById('videos');
-    videoList = JSON.parse(localStorage.getItem(name)) || [];
-    videoList.sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+    const videosList = document.getElementById('videos')
+    videoList = JSON.parse(localStorage.getItem(name)) || []
+    videoList.sort((a, b) => Date.parse(b.date) - Date.parse(a.date))
     videoList.forEach((item, i) => {
-        appendVideo(videosList, item, name, i);
-    });
+        appendVideo(videosList, item, name, i)
+    })
 }
 
 function appendVideo(videosList, video, name, id) {
-    const listItem = createVideoElement(video, name, id);
-    videosList.appendChild(listItem);
+    const listItem = createVideoElement(video, name, id)
+    videosList.appendChild(listItem)
 }
 
 function createVideoElement(video, name, id) {
-    const listItem = document.createElement('li');
-    const paragraphElement = document.createElement('p');
-    paragraphElement.innerHTML = `<a href="${video.link}" target="_blank">${video.title}</a> - (Published on ${video.date})`;
-    listItem.appendChild(paragraphElement);
-    const favoriteButton = document.createElement('button');
-    favoriteButton.textContent = video.isFavorite ? 'Remove Favorite' : 'Add to Favorites';
+    const listItem = document.createElement('li')
+    const paragraphElement = document.createElement('p')
+    video.link = video.link.replace("www.youtube.com", bestYoutubeSite)
+    paragraphElement.innerHTML = `<a href="${video.link}" target="_blank">${video.title}</a> - (Published on ${video.date})`
+    listItem.appendChild(paragraphElement)
+    const favoriteButton = document.createElement('button')
+    favoriteButton.textContent = video.isFavorite ? 'Remove Favorite' : 'Add to Favorites'
     favoriteButton.addEventListener('click', function () {
-        toggleFavorite(id, name);
+        toggleFavorite(id, name)
     });
-    listItem.appendChild(favoriteButton);
-    return listItem;
+    listItem.appendChild(favoriteButton)
+    return listItem
 }
 
 function toggleFavorite(id, name) {
     videoList[id].isFavorite = !videoList[id].isFavorite;
-    localStorage.setItem(name, JSON.stringify(videoList));
-    loadVideoById(id, name);
+    if (videoList[id].isFavorite){
+        favoriteList.push(videoList[id])
+        localStorage.setItem("favorite", JSON.stringify(favoriteList))
+    } else {
+        // favoriteList.push(videoList[id])
+    }
+    localStorage.setItem(name, JSON.stringify(videoList))
+    loadVideoById(id, name)
 }
 
 function loadVideoById(id, name) {
-    const videosList = document.getElementById('videos');
-    const itemToReplace = videosList.children[id];
+    const videosList = document.getElementById('videos')
+    const itemToReplace = videosList.children[id]
     if (itemToReplace) {
-        videosList.replaceChild(createVideoElement(videoList[id], name, id), itemToReplace);
+        videosList.replaceChild(createVideoElement(videoList[id], name, id), itemToReplace)
     }
 }
